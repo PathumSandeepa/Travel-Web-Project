0a55c191d52566bbb1ee1d70c0e18d4fca2cb40a
Pandula <vg.pandula@gmail.com>
2023-04-02 13:26:15 +0530

    updatefeedback

--- js/feedback.js
+++ js/feedback.js
@@ -18,3 +18,10 @@ function formValidate() {
     }
 }
 
+const feedbackForm = document.querySelector('form');
+const feedbackSelect = feedbackForm.querySelector('select');
+
+feedbackForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	alert(`Thank you for your ${feedbackSelect.value}!`);
+});
